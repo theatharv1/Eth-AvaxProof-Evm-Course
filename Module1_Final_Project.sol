3b942e5db4cc4d3c0e4f01f870b2a884e09ae439
Atharv Shrivastava <134605078+theatharv1@users.noreply.github.com>
2023-11-06 00:32:52 +0530

    Add files via upload

---
+++ Module1_Final_Project.sol
@@ -0,0 +1,28 @@
+/*
+Write a smart contract code that implements the require(), assert() and revert() statements.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ErrorHandling {
+    uint public balance = 0;
+
+    // Function to withdraw funds; checks if there's enough balance.
+    function withdraw(uint amount) public {
+        require(balance >= amount, "Insufficient balance for withdrawal");
+        balance -= amount;
+    }
+
+    // Function to deposit funds, ensures balance doesn't exceed 500.
+    function deposit(uint amount) public {
+        require(balance + amount <= 500, "Balance limit exceeded");
+        balance += amount;
+    }
+
+    // Function to check if the balance is zero.
+    function isBalanceZero() public view returns (string memory) {
+        assert(balance == 0);
+        return "Your balance is zero, no funds available for the transaction.";
+    }
+}
